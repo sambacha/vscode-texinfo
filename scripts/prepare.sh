9a1fd33f3fe9c8080ea9fb55ce4044199465999a
CismonX <admin@cismon.net>
2020-10-04 02:27:44 +0800

    Fix prepare.sh

--- scripts/prepare.sh
+++ scripts/prepare.sh
@@ -1,5 +1,7 @@
 #!/usr/bin/env sh
 
+mkdir -p ./out
+
 # Convert TextMate grammar from CSON to JSON (VSCode cannot recognize CSON ones).
 TMGRAMMAR_CSON=./node_modules/language-texinfo/grammars/texinfo.cson
 TMGRAMMAR_JSON=./out/texinfo.tmGrammar.json
