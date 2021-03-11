2d08ad4b88d51cea546dda39d4d42b2d68dc2fe6
CismonX <admin@cismon.net>
2021-03-11 12:08:35 +0800

    Do not include dependency license in vsix package.

--- scripts/prepare.sh
+++ scripts/prepare.sh
@@ -3,6 +3,5 @@
 SRC_PATH=./node_modules/language-texinfo
 DEST_PATH=./out/grammars
 mkdir -p $DEST_PATH
-cp $SRC_PATH/LICENSE.md $DEST_PATH
 # Convert TextMate grammar from CSON to JSON, as VSCode cannot recognize CSON ones.
 cson2json $SRC_PATH/grammars/texinfo.cson | json -j0 > $DEST_PATH/texinfo.json
