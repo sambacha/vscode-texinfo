5f7c15b309587a9397a0271ca3301f82a21c311d
CismonX <admin@cismon.net>
2021-03-16 20:01:13 +0800

    Relicense project to GPL.

--- README.md
+++ README.md
@@ -1,3 +1,11 @@
+<!--
+Copyright (C) 2020,2021  CismonX <admin@cismon.net>
+
+Copying and distribution of this file, with or without modification, are
+permitted in any medium without royalty, provided the copyright notice and
+this notice are preserved. This file is offered as-is, without any warranty.
+-->
+
 # vscode-texinfo
 
 [![Build Status](https://shields.io/drone/build/CismonX/vscode-texinfo?server=https%3A%2F%2Fdrone.cismon.net)](https://drone.cismon.net/CismonX/vscode-texinfo)
--- src/contexts/preview.ts
+++ src/contexts/preview.ts
@@ -1,8 +1,22 @@
 /**
  * contexts/preview.ts
- * 
- * @author CismonX <admin@cismon.net>
- * @license MIT
+ *
+ * Copyright (C) 2020,2021  CismonX <admin@cismon.net>
+ *
+ * This file is part of vscode-texinfo.
+ *
+ * vscode-texinfo is free software: you can redistribute it and/or modify it
+ * under the terms of the GNU General Public License as published by the Free
+ * Software Foundation, either version 3 of the License, or (at your option)
+ * any later version.
+ *
+ * vscode-texinfo is distributed in the hope that it will be useful, but
+ * WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY
+ * or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public License
+ * for more details.
+ *
+ * You should have received a copy of the GNU General Public License along with
+ * vscode-texinfo.  If not, see <https://www.gnu.org/licenses/>.
  */
 
 import * as path from 'path';
